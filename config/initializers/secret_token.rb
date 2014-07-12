4bc43056b37a261f26882ee8602b1df17eb6415f
James B Kemp <james@bibble.com>
2014-07-12 12:13:37 +0100

    more playing with secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,21 +10,21 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 
-SampleApp::Application.config.secret_key_base = 'f7d1d0cf1eba05cabb840d9af2b226c65a242e00720d994c0441ae8505e059a478cc347bb944dc97ae7b1d27a5d0fe0ea8836c364729b874708a32be94e91d62'
+# SampleApp::Application.config.secret_key_base = 'f7d1d0cf1eba05cabb840d9af2b226c65a242e00720d994c0441ae8505e059a478cc347bb944dc97ae7b1d27a5d0fe0ea8836c364729b874708a32be94e91d62'
 
-# require 'securerandom'
+require 'securerandom'
 
-# def secure_token 
-# 	token_file = Rails.root.join('.secret')
-# 	if File.exist?(token_file)
-# 		# Use the existing token.
-# 		File.read(token_file).chomp
-# 	else
-# 		#generate a new token and store it in token_file.
-# 		token = SecureRandom.hex(64)
-# 		File.write(token_file, token)
-# 		token
-# 	end
-# end
+def secure_token 
+	token_file = Rails.root.join('.secret')
+	if File.exist?(token_file)
+		# Use the existing token.
+		File.read(token_file).chomp
+	else
+		#generate a new token and store it in token_file.
+		token = SecureRandom.hex(64)
+		File.write(token_file, token)
+		token
+	end
+end
 
-# SampleApp::Application.config.secret_key_base = secure_token
+SampleApp::Application.config.secret_key_base = secure_token
