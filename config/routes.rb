918c57c5defdb550b27e7a244b56c2a14a0c60c6
James B Kemp <james@bibble.com>
2014-07-16 22:44:53 +0100

    signup page all complete and passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 SampleApp::Application.routes.draw do
   get "users/new"
   root 'static_pages#home'
-  # match '/home',    to: 'static_pages#home',    via: 'get'
+  
+  match '/signup',  to: 'users#new',            via: 'get'
 
   match '/help',    to: 'static_pages#help',    via: 'get'
 
