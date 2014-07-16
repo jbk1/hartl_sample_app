dbc5b023f19e90f97577460ab2b5fab191554f1f
James B Kemp <james@bibble.com>
2014-07-16 22:58:55 +0100

    5.6 ex1 done

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -25,7 +25,7 @@ describe "Static pages" do
 
   describe "Contact page" do
     before { visit contact_path }
-    it { should have_content('contact me at: james@bibble.com') }
+    it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact')) }
   end
 
