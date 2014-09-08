76d4c7373f462ae5a42e80836791ed9876099715
James B Kemp <james@bibble.com>
2014-09-08 13:21:52 +0800

    adding gravatar size parameter

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,7 +21,15 @@ describe "UserPages" do
   		it "should not create a user" do
   			expect { click_button submit }.not_to change(User, :count)
   		end
-  	end
+  	
+      # describe "after submission" do
+      #   before { click_button submit }
+
+      #   it { should have_selector('title', text: 'Sign up') }
+      #   it { should have_content('error') }
+      # end
+
+    end
 
   	describe"with valid information" do
   		before do 
