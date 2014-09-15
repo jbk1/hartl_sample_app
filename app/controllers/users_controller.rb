07824efbe85fc94afe08ae06f4acf3fc0e98b4a3
James B Kemp <james@bibble.com>
2014-09-15 17:20:56 +0800

    Completing 10.3.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,13 +59,6 @@ private
   end
 
 # before filters
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in." 
-    end
-  end
-
 
   def correct_user
     @user = User.find(params[:id])
